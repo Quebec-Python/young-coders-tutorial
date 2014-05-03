f5d1507383d779ad1226854342627e2883ac0880
bchhun <bernard.chhun@gmail.com>
2014-05-03 13:13:09 -0400

    restructuring the project's folder as a python package for later pypi release

---
+++ setup.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name = 'youngcoders',
+    packages = ['youngcoders'],
+    version = '0.1',
+    description = 'Slides and resources for the Young Coders tutorial',
+    author = 'Barbara Shaurette',
+    author_email = 'barbara.shaurette@gmail.com',
+    url = 'https://github.com/mechanicalgirl/young-coders-tutorial',
+    download_url = 'https://github.com/mechanicalgirl/young-coders-tutorial/tarball/0.1',
+    keywords = ['young', 'coders', 'resources', 'slides', 'youngcoders', 'tutorial'],
+    classifiers = [],
+)
